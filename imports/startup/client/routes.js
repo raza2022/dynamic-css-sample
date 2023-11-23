2d7f74069a7bd5066c5ae129f87a7430d6e27f8e
Abdul Hameed <raza2022@gmail.com>
2023-11-23 12:49:13 +0500

    2nd sample copied

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,12 +3,20 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 import '../../ui/layouts/body/body_clean_survey.js';
 import '../../ui/pages/responses/response.js'
+import '../../ui/pages/responses/response1.js'
 import '../../ui/pages/responses/childs/responseStep2'
 
 FlowRouter.route('/', {
   name: 'response',
   action() {
     this.render('clean_layout', 'response');
+  }
+});
+
+FlowRouter.route('/v1', {
+  name: 'response1',
+  action() {
+    this.render('clean_layout', 'response1');
   }
 });
 
